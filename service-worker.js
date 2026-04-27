7b30830d09c2a4c2416037683a56c1d76f7cbbea
saenzt1-hub <saenzt1@my.smccd.edu>
2026-04-27 12:04:53 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
 const CACHE_NAME = "pwa-cache-v1";
 
 const FILES_TO_CACHE = [
-    "/",
-    "index.html",
-    "styles.css",
-    "app.js",
-    "lightblue.jpg",
-    "lightgold.jpg",
-    "192.png",
-    "512.png"
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./app.js",
+    "./lightblue.jpg",
+    "./lightgold.jpg",
+    "./192.png",
+    "./512.png"
 ];
 
 self.addEventListener("install", event => {
